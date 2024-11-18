502f5da2fb7eadf750d5f028c48decad33880fc0
GB <gabrielebardi.gb@gmail.com>
2024-11-18 17:39:50 +0100

    Set up iOS app project with SwiftUI

---
+++ NoSwipeApp/NoSwipeApp/NoSwipeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NoSwipeAppApp.swift
+//  NoSwipeApp
+//
+//  Created by G B on 11/18/2024.
+//
+
+import SwiftUI
+
+@main
+struct NoSwipeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ NoSwipeApp/NoSwipeAppTests/NoSwipeAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  NoSwipeAppTests.swift
+//  NoSwipeAppTests
+//
+//  Created by G B on 11/18/2024.
+//
+
+import Testing
+@testable import NoSwipeApp
+
+struct NoSwipeAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
